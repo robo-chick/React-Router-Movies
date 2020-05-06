4d6e9190cb2b9e65e6e3a7658a8e040b935a2fab
Tasha <tashingsworth@gmail.com>
2020-05-06 11:04:24 -0400

    wrapped app with router and added 2 routes. Created link in MovieList

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -28,7 +30,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/' component={MovieList} />
+      <Route path='/movies.id' component={Movie} />
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const MovieList = props => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    axios
+    .get('http://localhost:5000/api/movies')
+    .then(response => {
+      setMovies(response.data)
+    })
+    .catch(error => console.log(error));
+  }, [])
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+      {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}> <MovieDetails key={movie.id} movie={movie} /></Link>
       ))}
     </div>
   );
